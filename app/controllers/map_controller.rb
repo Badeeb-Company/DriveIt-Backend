b3e9504918f5892db50f8f4d04d60c2f0a258d8e
Mostafa <mostafa.zonkoly@inovaeg.com>
2017-10-02 01:01:51 +0200

    Track Zoom + Page size

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,8 +1,8 @@
 class MapController < ApplicationController
-    WillPaginate.per_page = 20
+    
 
 	def map
-		@drivers_table = Driver.order(id: :ASC).paginate(:page => params[:page])
+		@drivers_table = Driver.order(id: :ASC).paginate(:page => params[:page],per_page: 10)
 		@drivers = Driver.all.order(id: :ASC)
 		render :map
 	end
