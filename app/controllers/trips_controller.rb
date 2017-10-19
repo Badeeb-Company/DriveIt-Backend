0812fca55b45804187a940177a5e9293b5ba8af4
meldeeb <mohammad.edeeb@gmail.com>
2017-10-19 05:50:53 +0200

    Add pagination to trips

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -4,7 +4,7 @@ class TripsController < ApplicationController
   # GET /trips
   # GET /trips.json
   def index
-    @trips = Trip.all
+    @trips = Trip.paginate(:page => params[:page], :per_page => 10).order("id desc")
   end
 
   # GET /trips/1
