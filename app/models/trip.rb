b9aeb4e3371739000cd537083408e732cb4d457e
meldeeb <mohammad.edeeb@gmail.com>
2017-10-19 06:18:35 +0200

    Enhance trips views

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -16,10 +16,45 @@ class Trip < ApplicationRecord
 	5) CANCELLED
 	6) REJECTED
 =end
+
+	def trip_state_string
+		if driver
+			case self.trip_state
+			when 1
+				'Pending'
+			when 2
+				'Expired'
+			when 3
+				'In Progress'
+			when 4
+				'Completed'
+			when 5
+				'Cancelled'
+			when 6
+				'Rejected'
+			end
+		else
+			case self.trip_state
+			when 1
+				'Pending'
+			when 2
+				'Accepted'
+			when 3
+				'Not served'
+			end
+		end
+	end
+
+	def driver_name
+		return nil if self.driver_id.blank?
+		return Driver.where(:id => self.driver_id).first.name
+	end
+
 	def driver
 		return nil if self.driver_id.blank?
 		return Driver.where(:id => self.driver_id).first
 	end
+
 	def as_json (options = {})
 		json = super.as_json(options)
 		json["trip_state"] = Trip.trip_states.keys[json["trip_state"].to_i]
