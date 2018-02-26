9eda1719ecf2753aace2599c32bb6caca0673556
meldeeb <mohammad.edeeb@gmail.com>
2018-02-26 22:05:48 +0200

    Show only online drivers on map

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -2,8 +2,11 @@ class MapController < ApplicationController
     
 
 	def map
-		@drivers_table = Driver.order(id: :ASC).paginate(:page => params[:page],per_page: 10)
-		@drivers = Driver.all.order(id: :ASC)
+		@drivers_table = Driver.where(driver_availability: 0)
+			.order(id: :ASC)
+			.paginate(:page => params[:page],per_page: 10)
+			
+		@drivers = Driver.where(driver_availability: 0)
 		render :map
 	end
 
